8b958331046142abe358a1f78c63d37fb9eb7617
Nikita Klyukoit <klukoyt@dterra.ru>
2024-04-04 18:46:55 +0300

    add layout style

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,18 +6,28 @@ function App() {
 
   return (
     <div className={style.app}>
-      <p className={style.app__title}>App title</p>
-      <p className={style.app__descr}>App description</p>
-      <p className={style.app__count}>App counter: {count}</p>
-      <button
-        type="button"
-        className={style.app__button}
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Нажми меня
-      </button>
+      <header>
+        <div className={style.inner}>header</div>
+      </header>
+      <main>
+        <div className={style.inner}>
+          <p className={style.app__title}>App title</p>
+          <p className={style.app__descr}>App description</p>
+          <p className={style.app__count}>App counter: {count}</p>
+          <button
+            type="button"
+            className={style.app__button}
+            onClick={() => {
+              setCount(count + 1);
+            }}
+          >
+            Нажми меня
+          </button>
+        </div>
+      </main>
+      <footer>
+        <div className={style.inner}>footer</div>
+      </footer>
     </div>
   );
 }
