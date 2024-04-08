c7d15fe8a1736b5b1d7253bcc817166111ad0534
Nikita Klyukoit <klukoyt@dterra.ru>
2024-04-08 13:25:50 +0300

    add FullInfo widget

--- src/pages/MainPage/Main.tsx
+++ src/pages/MainPage/Main.tsx
@@ -3,6 +3,7 @@ import SwitcherTheme from '../../shared/switcherTheme';
 import { Today } from '../../widgets/today';
 import { Prediction } from '../../widgets/prediction';
 import { HourlyForecast } from '../../widgets/HourlyForecast';
+import { FullInfo } from '../../widgets/FullInfo';
 
 const MainPage = () => (
   <div>
@@ -13,7 +14,7 @@ const MainPage = () => (
       <section className="section">
         <div className={style.main__wrapper}>
           <Today />
-          <div>Блок 2</div>
+          <FullInfo />
         </div>
       </section>
       <section className="section">
