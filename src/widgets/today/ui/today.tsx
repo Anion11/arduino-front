736d35137710c8c0f12a94b62ad46ccfe17fa9ce
Nikita Klyukoit <klukoyt@dterra.ru>
2024-05-16 17:47:43 +0300

    upd main page

--- src/widgets/today/ui/today.tsx
+++ src/widgets/today/ui/today.tsx
@@ -1,9 +1,10 @@
+import Сity from '../../../features/city';
+import Date from '../../../features/currentDate';
 import style from './today.module.scss';
 // eslint-disable-next-line import/prefer-default-export
 export const Today = () => (
   <div className={style.today}>
-    <div className={style.today__city}>Athens</div>
-    <div className={style.today__time}>09:03</div>
-    <div className={style.today__date}>Thursday, 31 Aug</div>
+    <Сity />
+    <Date />
   </div>
 );
